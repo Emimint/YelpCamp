a4c2fb20d33d0112d0d62230968e7ed7685d3257
Emimint <90863470+Emimint@users.noreply.github.com>
2024-01-23 15:39:58 -0500

    add author comment filed

--- seeds/index.js
+++ seeds/index.js
@@ -26,6 +26,7 @@ const seedDB = async () => {
   for (let i = 0; i < 50; i++) {
     const random1000 = Math.floor(Math.random() * 1000);
     const camp = new Campground({
+      author: '65b017880a5532f0ffca6b11',
       location: `${cities[random1000].city}, ${cities[random1000].state}`,
       title: `${sample( descriptors )} ${sample( places )}`,
       image: 'https://source.unsplash.com/collection/483251/800x400',
